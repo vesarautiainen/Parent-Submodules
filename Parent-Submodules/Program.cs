a75448ae6c85567ce2dd69eaf88782ac06dc55f1
Taysser <tagherfa@microsoft.com>
2020-08-29 16:09:40 -0700

    Add project files.

---
+++ Parent-Submodules/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parent_Submodules
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
